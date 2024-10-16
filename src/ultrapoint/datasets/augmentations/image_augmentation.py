Normalization fix

src/ultrapoint/datasets/augmentations/image_augmentation.py
@@ -29,14 +29,10 @@ class ImageAugmentation:
     def __call__(
         self,
         image: np.ndarray,
-        normalize: bool = False,
+        normalize: bool = True,
         grayscale: bool = False,
         **config
     ):
-        # TODO: ???
-        if image.dtype != np.uint8:
-            image = (image * 255).astype(np.uint8)
-
         if len(image.shape) == 2:
             grayscale = True
             image = image[:, :, np.newaxis]

src/ultrapoint/datasets/images_dataset.py
@@ -71,7 +71,7 @@ class ImagesDataset(Dataset):
         if (self._enable_photo_train is True and self._mode == "train") or (
             self._enable_photo_val and self._mode == "val"
         ):
-            img_aug = self._augmentation(img_aug)
+            img_aug = self._augmentation(img_aug, normalize=True)
 
         img_aug = torch.tensor(img_aug, dtype=torch.float32).view(-1, H, W)
 